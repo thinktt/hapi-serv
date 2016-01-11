4e8965a56138f95fb14636d7c588701a79a237be
thinktt <github@thinktt.e4ward.com>
2016-01-10 19:11:06 -0600

    first commit - Hapi app that says Howdy
    This commit creates a basic hellow world Hapi app.

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var Hapi = require('hapi'); 
+var server = new Hapi.Server(); 
+
+server.connection({port: 3000});
+
+server.start(function() {
+	console.log('Server running at:', server.info.uri);
+});
+
+server.route({
+	method: 'GET',
+	path: '/',
+	handler: function(request, reply) {
+		reply('Howdy!');
+	}
+});
