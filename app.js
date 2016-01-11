4d02c821a47cbb6e2671a04f505a13d81f993277
thinktt <github@thinktt.e4ward.com>
2016-01-10 23:27:36 -0600

    gets logging working breaks other things

--- app.js
+++ app.js
@@ -2,17 +2,62 @@
 
 var Hapi = require('hapi'); 
 var server = new Hapi.Server(); 
+var Path = require('path');
+var goodConfig;
 
-server.connection({port: 3000});
+
+server.connection({
+	port: 3000
+});
 
 server.start(function() {
 	console.log('Server running at:', server.info.uri);
 });
 
+//............Routes................
 server.route({
 	method: 'GET',
-	path: '/',
+	path: '/greeting',
 	handler: function(request, reply) {
 		reply('Howdy!');
 	}
 });
+
+server.route({
+    method: 'GET',
+    path: '/matrix/{param*}',
+    handler: {
+        directory: {
+            path: 'public'
+        }
+    }
+});
+
+
+//............Logging..............
+goodConfig = {
+	opsInterval: 1000,
+	reporters: [{
+		reporter: require('good-console'),
+		events: { log: '*', response: '*' }
+	}]
+};
+
+server.register({
+	register: require('good'),
+	options: goodConfig
+}, 
+function (err) {
+	if (err) console.error(err);
+});
+
+server.register(require('inert'), (err) => {
+	if (err) {
+		throw err;
+	}
+});
+
+
+
+
+
